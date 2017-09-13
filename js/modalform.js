6e41cdf380817421d88cd036e48be280acfa45b6
Shamonix <30830992+Shamonix@users.noreply.github.com>
2017-09-13 19:33:24 +0300

    Финал

---
+++ js/modalform.js
@@ -0,0 +1,44 @@
+var link = document.querySelector(".footer-button");
+      var popup = document.querySelector(".write-us");
+      var close = popup.querySelector(".modal-close");
+      var form = popup.querySelector("form");
+      var username = popup.querySelector("[name=name]");
+      var email = popup.querySelector("[name=mail]");
+      var storage = localStorage.getItem("username");
+
+      link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal-show");
+        if (storage) {
+          username.value = storage;
+          email.focus();
+        } else {
+          username.focus();
+        }
+      });
+
+      close.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      });
+
+      form.addEventListener("submit", function (evt){
+        if (!username.value || !email.value) {
+        evt.preventDefault();
+        console.log("Нужно ввести имя и электронную почту");
+        popup.classList.add("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        } else {
+          localStorage.setItem("username", username.value);
+        }
+      });
+
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+          }
+        }
+      });
